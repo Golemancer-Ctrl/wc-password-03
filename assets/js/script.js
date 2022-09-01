dc6ce12d1e37c7cd1366313f199254e586603cf1
Eric Marcy <ericmarcypro@gmail.com>
2022-08-31 20:14:10 -0400

    added user queries, refactored length code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,40 @@
 function generatePassword() {
   
   // pool of alphanumeric characters and symbols to pull from
-  var alphanum = ("abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()");
   
-  // container string for the generated password
+  var lowercase = ("abcdefghijklmnopqrstuvwxyz");
+  var numList = ("1234567890");
+  var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var symbols = ("!@#$%^&*()?");
+  
+  var alphanum = ("");
   var password = "";
 
-  // sets a randomized length for the password between 8 and 15 characters
-  var rLength = Math.floor(Math.random() * (15-8) + 8);
+  // user prompts for pw criteria (numbers, case size, symbols, and length)
+  var num = confirm("Do you want numbers?");
+  var low = confirm("Do you want lowercase letters?");
+  var up = confirm("Do you want uppercase letters?");
+  var sym = confirm("Do you want symbols?");
+
+  var long = Number.parseInt(window.prompt("Enter the number of characters you want your password to have.  Minimum 8.  Maximum 128."));
+
+
+  // sets length for the password to between 8 and 128 characters
+  if (long > 128) {
+    var rLength = 127;
+    alert("Maximum character limit 128!");
+  } else if (long < 8) {
+    var rLength = 7;
+    alert("Minimum character limit 8!");
+  } else {
+    var rLength = long - 1;
+  }
+
+  //if statements determining the pool of potential characters to draw from
+  if (num) var alphanum = alphanum + numList;
+  if (low) var alphanum = alphanum + lowercase;
+  if (up) var alphanum = alphanum + uppercase;
+  if (sym) var alphanum = alphanum + symbols;
   
   // for loop to pull randomly selected alphanumeric chars & symbols from alphanum to generate the password  
   for (var index = 0; index <= rLength; index++) {
